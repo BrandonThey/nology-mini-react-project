812d2b17fe9b222995756e8ef843e1227422cbc8
Brandon They <theyb5220@gmail.com>
2022-08-15 10:05:39 -0500

    Added use state "counter"and use increment and decrement handle fuctions to the counter

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,10 +1,21 @@
 import "./Counter.scss";
 
-const Counter = () => {
+import { useState } from "react";
 
+const Counter = () => {
+    const [counter, setCounter] = useState(0);
+
+    const handleIncrement = () => {
+        setCounter(counter+1);
+    }
+
+    const handleDecrement = () => {
+        setCounter(counter-1);
+    }
+    
     return (
         <div>
-            
+
         </div>
     )
 }
